570e06874c70ad1ea71cb8ef6ca2557d6e6ec883
Lainie Wright <lainiewright@gmail.com>
2016-02-07 21:08:53 -0500

    Search by restaurant name

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, FiltersViewControllerDelegate {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, FiltersViewControllerDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var businesses: [Business]!
+    var filteredData: [Business]!
+    var searchActive: Bool! = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,31 +24,58 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
-
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
         
-            for business in businesses {
-                print(business.name!)
-                print(business.address!)
-            }
-        })
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+        filteredData = businesses
+
+//        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+//            self.businesses = businesses
+//        
+//            for business in businesses {
+//                print(business.name!)
+//                print(business.address!)
+//            }
+//        })
         
 
         //Example of Yelp search with more search options specified
         Business.searchWithTerm("Restaurants", sort: .Distance, categories: nil, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             self.tableView.reloadData()
-            
-//            for business in businesses {
-//                print(business.name!)
-//                print(business.address!)
-//            }
         }
     }
     
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if let businesses = businesses {
+            filteredData = searchText.isEmpty ? businesses : businesses.filter({(data: Business) -> Bool in
+                return data.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+            })
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        filteredData = businesses
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if businesses != nil {
+        if let filtered = filteredData {
+            return filtered.count
+        } else if businesses != nil {
             return businesses.count
         } else {
             return 0
@@ -54,7 +84,12 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
-        cell.business = businesses[indexPath.row]
+        
+        if filteredData != nil {
+            cell.business = filteredData[indexPath.row]
+        } else {
+            cell.business = businesses[indexPath.row]
+        }
         return cell
     }
 
